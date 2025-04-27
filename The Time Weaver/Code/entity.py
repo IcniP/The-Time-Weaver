7b896ec7a72ef4e297f605db65762a2dac2b79eb
Pinci <81608534+IcniP@users.noreply.github.com>
2025-04-27 22:56:24 +0700

    Add files via upload

--- The Time Weaver/Code/entity.py
+++ The Time Weaver/Code/entity.py
@@ -1,66 +1,92 @@
+import pygame
 from settings import *
+from os import listdir
+from os.path import join
 
-
-class allsprite(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-        self.Screen = pygame.display.get_surface()
-        self.offset = pygame.Vector2()
-
-    def draw(self,target_pos):
-        self.offset.x = -(target_pos[0] - WINDOW_WIDTH/ 2)
-        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT/ 2)
-        ground_sprite = [sprite for sprite in self if hasattr(sprite, 'ground')]
-        object_sprite  = [sprite for sprite in self if not hasattr(sprite, 'ground')]
-        
-        for layer in [ground_sprite,object_sprite]:
-            for sprite in sorted(layer,key =lambda sprite: sprite.rect.centery):
-                self.Screen.blit(sprite.image,sprite.rect.topleft + self.offset)
-
-class player(pygame.sprite.Sprite):
-    def __init__(self,pos, groups , collision_sprite):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
         super().__init__(groups)
- 
-        self.load_image()
-        self.state, self.frame_index = 'down',0
-        self.image = pygame.image.load(join('Assets','player','idle','1.png')).convert_alpha()
-        self.rect = self.image.get_frect(center=pos)
-        self.hitbox_rect = self.rect.inflate(-60,-90)
-        self.speed = 500
-        self.direction = pygame.Vector2(0,0)
-        self.collision_sprite = collision_sprite         
+        self.animations = {'Idle': [], 'Move': [], 'Attack': [], 'Jump': []}
+        self.import_assets()
+        self.status = 'Idle'
+        self.frame_index = 0
+        self.animation_speed = 6
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=pos)
+
+        # movement
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 200  # pixels per second
+        self.gravity = 500
+        self.jump_speed = -300
+
+        # attack
+        self.attacking = False
+
+    def import_assets(self):
+        base_path = r'E:\Coding\python\The Time Weaver\Assets\Player'
+        for action in self.animations.keys():
+            full_path = join(base_path, action)
+            self.animations[action] = self.import_folder(full_path)
+
+    def import_folder(self, path):
+        images = []
+        for file_name in sorted(listdir(path), key=lambda x: int(x.split('.')[0])):
+            full_path = join(path, file_name)
+            image = pygame.image.load(full_path).convert_alpha()
+            images.append(image)
+        return images
 
     def input(self):
         keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_d] or keys[pygame.K_RIGHT]) - int(keys[pygame.K_a]or keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_s]or keys[pygame.K_DOWN]) - int(keys[pygame.K_w]or keys[pygame.K_UP])
-        if self.direction.length() > 0 :
-            self.direction = self.direction.normalize()
+        mouse_pressed = pygame.mouse.get_pressed()
 
-    def move(self,dt):
-        self.hitbox_rect.x += self.direction.x * self.speed * dt
-        self.collision('horizontal')
-        self.hitbox_rect.y += self.direction.y * self.speed * dt
-        self.collision('vertical')
-        self.rect.center = self.hitbox_rect.center
+        # Reset horizontal direction
+        self.direction.x = 0
 
-    def collision(self, direction):
-        for sprite in self.collision_sprite:
-            if sprite.rect.colliderect(self.hitbox_rect):
-                if direction == 'horizontal':
-                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
-                    if self.direction.x < 0 : self.hitbox_rect.left = sprite.rect.right
-                if direction == 'vertical':
-                    if self.direction.y > 0 : self.hitbox_rect.bottom = sprite.rect.top
-                    if self.direction.y < 0 : self.hitbox_rect.top = sprite.rect.bottom
+        if mouse_pressed[0]:  # Left mouse button
+            self.attacking = True
+        elif keys[pygame.K_SPACE]:
+            if self.on_ground():
+                self.direction.y = self.jump_speed
+        else:
+            self.attacking = False
+            if keys[pygame.K_a]:
+                self.direction.x = -1
+            if keys[pygame.K_d]:
+                self.direction.x = 1
 
-    def update(self,dt):
+    def on_ground(self):
+        return self.rect.bottom >= WINDOW_HEIGHT  # Assume ground is bottom of screen
+
+    def move(self, dt):
+        self.direction.y += self.gravity * dt  # Apply gravity
+        self.rect.x += self.direction.x * self.speed * dt
+        self.rect.y += self.direction.y * dt
+
+        # Floor collision
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+            self.direction.y = 0
+
+    def animate(self, dt):
+        if self.attacking:
+            self.status = 'Attack'
+        elif self.direction.y < 0:
+            self.status = 'Jump'
+        elif self.direction.x != 0:
+            self.status = 'Move'
+        else:
+            self.status = 'Idle'
+
+        frames = self.animations[self.status]
+        self.frame_index += self.animation_speed * dt
+        if self.frame_index >= len(frames):
+            self.frame_index = 0
+        self.image = frames[int(self.frame_index)]
+
+    def update(self, dt):
         self.input()
         self.move(dt)
-        
-
-
-
-
-
-
+        self.animate(dt)
--- The Time Weaver/Code/main.py
+++ The Time Weaver/Code/main.py
@@ -1,12 +1,10 @@
-from settings import * 
-from entity import *
-from entity import player
-from entity import allsprite
+from settings import *
+from entity import Player
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.Screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Time Weaver')
         self.clock = pygame.time.Clock()
         self.running = True
@@ -15,28 +13,27 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
-        
-
+        # add player
+        self.player = Player((WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2), self.all_sprites)
 
     def run(self):
         while self.running:
-            dt = self.clock.tick(FRAMERATE) / 1000 
+            dt = self.clock.tick(FRAMERATE) / 1000
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False 
-            
+                    self.running = False
+
             # update
             self.all_sprites.update(dt)
 
             # draw 
-            self.Screen.fill(BG_COLOR)
-            self.all_sprites.draw(self.Screen)
-            self.all_sprites.update(dt)
+            self.screen.fill(BG_COLOR)
+            self.all_sprites.draw(self.screen)
             pygame.display.update()
         
         pygame.quit()
 
 if __name__ == '__main__':
     game = Game()
-    game.run() 
+    game.run()
