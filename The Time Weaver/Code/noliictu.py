3cc0c083c873587d735133ca8192053749d8e891
Ondor-R <rehanondor@gmail.com>
2025-05-07 16:52:33 +0700

    Update noliictu.py

--- The Time Weaver/Code/noliictu.py
+++ The Time Weaver/Code/noliictu.py
@@ -1,3 +1,4 @@
+import pygame
 from settings import *
 from entity import *
 from bossbase import BossBase
@@ -25,7 +26,7 @@ class Knive(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
         self.speed = 320 #projectile speed
-        self.damage = 1  # Damage ke player
+        self.damage = 0  # Damage ke player
 
     def update(self, dt):
         move = self.direction * self.speed * dt
@@ -65,7 +66,7 @@ class Noliictu(BossBase):
         self.hitbox.center = self.rect.center
 
         self.hp = 3000
-        self.detection_rect = pygame.Rect(self.hitbox.centerx - 100, self.hitbox.centery - 100, 200, 200)
+        self.detection_rect = pygame.Rect(self.rect.centerx - 100, self.rect.centery - 100, 200, 200)
         self.detection_active = False
         self.time_in_rect = 0
         self.teleporting = False
@@ -98,7 +99,6 @@ class Noliictu(BossBase):
         # after ult teleport
         self.after_ult_teleport = False
 
-
     def ult(self):
         self.is_uliting = True
         self.ult_timer = 0
@@ -107,7 +107,7 @@ class Noliictu(BossBase):
         for marker in map.get_layer_by_name('entities'):
             if marker.name == 'Ult':
                 teleport_pos = (marker.x, marker.y)
-                self.hitbox.center = teleport_pos
+                self.rect.center = teleport_pos
                 break
             
     def attack(self, dt):
@@ -122,8 +122,8 @@ class Noliictu(BossBase):
             self.knife_timer = 0
             self.knives_fired += 1
 
-            offset = pygame.Vector2(70, 0) if self.player.rect.centerx > self.hitbox.centerx else pygame.Vector2(-70, 0)
-            spawn_pos = pygame.Vector2(self.hitbox.center) - offset
+            offset = pygame.Vector2(70, 0) if self.player.rect.centerx > self.rect.centerx else pygame.Vector2(-70, 0)
+            spawn_pos = pygame.Vector2(self.rect.center) - offset
             Knive(spawn_pos, self.player.player_hitbox.midbottom, [self.knive_group], self.player)
 
         if self.knives_fired >= self.max_knives:
@@ -133,7 +133,7 @@ class Noliictu(BossBase):
 
     def update(self, dt):
         super().update(dt)
-        self.detection_rect.center = self.hitbox.center
+        self.detection_rect.center = self.rect.center
         num = random.randint(1, 9)
 
         if self.dying:
@@ -178,7 +178,7 @@ class Noliictu(BossBase):
                 if possible_markers:
                     teleport_marker = random.choice(possible_markers)
                     teleport_pos = (teleport_marker.x, teleport_marker.y)
-                    self.hitbox.center = teleport_pos
+                    self.rect.center = teleport_pos
                 self.play_animation('TeleportIn')
             elif self.status == 'TeleportIn' and self.frame_index >= len(frames) - 1:
                 self.after_ult_teleport = False
@@ -201,7 +201,7 @@ class Noliictu(BossBase):
                 for marker in map.get_layer_by_name('entities'):
                     if marker.name == f'{num}':
                         teleport_pos = (marker.x, marker.y)
-                        self.hitbox.center = teleport_pos
+                        self.rect.center = teleport_pos
                         break
                 self.play_animation('TeleportIn')
             elif self.status == 'TeleportIn' and self.frame_index >= len(frames) - 1:
