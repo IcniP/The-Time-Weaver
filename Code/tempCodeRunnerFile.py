7d0652bbc88cd16cccb087aa509db17fb6436b98
Ondor-R <rehanondor@gmail.com>
2025-05-09 05:58:48 +0700

    Cleanin' Entity

---
+++ Code/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+self.patrol_zones = []
+        # for obj in map.get_layer_by_name('zones'):
+        #     rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+        #     self.patrol_zones.append(rect)
