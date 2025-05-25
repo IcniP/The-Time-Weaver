5d1e6b284b74c5800998f1c51573e90a3cd63bf6
Muhammad Royhan Alfitra <143269666+SmollJesteR@users.noreply.github.com>
2025-05-25 20:50:37 +0700

    Update interface.py
    Healthbar

--- Code/interface.py
+++ Code/interface.py
@@ -61,3 +61,43 @@ class UserInterface:
             max_value = player.max_potions,
             position = (64, 20)
         )
+
+class BossHealthBar:
+    def __init__(self, max_health, length, pos, name='Boss'):
+        self.max_health = max_health
+        self.current_health = max_health
+        self.target_health = max_health
+        self.health_bar_length = length
+        self.health_ratio = self.max_health / self.health_bar_length
+        self.health_change_speed = 5
+        self.pos = pos  # (x, y)
+        self.name = name
+
+    def update(self, current_hp):
+        self.target_health = max(0, min(current_hp, self.max_health))
+
+    def draw(self, surface):
+        transition_width = 0
+        transition_color = (255, 0, 0)
+
+        if self.current_health < self.target_health:
+            self.current_health += self.health_change_speed
+            transition_width = int((self.target_health - self.current_health) / self.health_ratio)
+            transition_color = (0, 255, 0)
+        elif self.current_health > self.target_health:
+            self.current_health -= self.health_change_speed
+            transition_width = int((self.target_health - self.current_health) / self.health_ratio)
+            transition_color = (255, 255, 0)
+
+        bar_width = int(self.current_health / self.health_ratio)
+        bar_rect = pygame.Rect(self.pos[0], self.pos[1], bar_width, 12)
+        transition_rect = pygame.Rect(bar_rect.right, self.pos[1], transition_width, 12)
+
+        pygame.draw.rect(surface, (255, 0, 0), bar_rect)
+        pygame.draw.rect(surface, transition_color, transition_rect)
+        pygame.draw.rect(surface, (255, 255, 255), (self.pos[0], self.pos[1], self.health_bar_length, 12), 3)
+
+        font = pygame.font.Font('Assets/Fonts/m5x7.ttf', 20)
+        text_surface = font.render(self.name, True, (255, 255, 255))
+        text_rect = text_surface.get_rect(midbottom=(self.pos[0] + self.health_bar_length // 2, self.pos[1] - 5))
+        surface.blit(text_surface, text_rect)
